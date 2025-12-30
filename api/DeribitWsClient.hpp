628452e632926151eff5d84da13f7de74d4ad152
PAAR16 <parthshe7@gma.com>
2025-12-30 11:46:28 +0530

    Introduce Deribit API abstraction using interface-based design

--- api/DeribitWsClient.hpp
+++ api/DeribitWsClient.hpp
@@ -1,30 +1,9 @@
-#ifndef DERIBIT_WS_CLIENT_HPP
-#define DERIBIT_WS_CLIENT_HPP
-
+#pragma once
 #include "IDeribitApi.hpp"
-#include <string>
-#include <functional>
-#include <thread>
-#include <atomic>
 
 class DeribitWsClient : public IDeribitApi {
 public:
-    DeribitWsClient();
-    ~DeribitWsClient() override;
-
-    void connect() override;
-    void disconnect() override;
-    void subscribe(const std::string& channel) override;
-    void unsubscribe(const std::string& channel) override;
-    void sendRequest(const std::string& request, std::function<void(const std::string&)> callback) override;
-
-private:
-    void run();
-    void onMessage(const std::string& message);
-
-    std::thread worker_;
-    std::atomic<bool> running_;
-    // Placeholder for websocket connection
+    void connect(bool testnet) override;
+    void authenticate(const std::string&, const std::string&) override;
+    void placeOrder(const Order&) override;
 };
-
-#endif // DERIBIT_WS_CLIENT_HPP
--- api/IDeribitApi.hpp
+++ api/IDeribitApi.hpp
@@ -1,18 +1,10 @@
-#ifndef IDERIBIT_API_HPP
-#define IDERIBIT_API_HPP
-
-#include <string>
-#include <functional>
+#pragma once
+#include "core/Order.hpp"
 
 class IDeribitApi {
 public:
+    virtual void connect(bool testnet) = 0;
+    virtual void authenticate(const std::string&, const std::string&) = 0;
+    virtual void placeOrder(const Order&) = 0;
     virtual ~IDeribitApi() = default;
-
-    virtual void connect() = 0;
-    virtual void disconnect() = 0;
-    virtual void subscribe(const std::string& channel) = 0;
-    virtual void unsubscribe(const std::string& channel) = 0;
-    virtual void sendRequest(const std::string& request, std::function<void(const std::string&)> callback) = 0;
 };
-
-#endif // IDERIBIT_API_HPP
